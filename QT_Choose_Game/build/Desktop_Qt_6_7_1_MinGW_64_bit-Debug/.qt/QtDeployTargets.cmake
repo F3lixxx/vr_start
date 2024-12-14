604c2501fd7449f96406c4259a2c9dc4beb8573b
“Ruzmetov” <f3lixxx@yandex.ru>
2024-12-14 10:26:43 +0300

    QT_UI

---
+++ QT_Choose_Game/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Choose_Game_FILE C:/Users/User/Documents/QT_Projects/Choose_Game/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/Choose_Game.exe)
+set(__QT_DEPLOY_TARGET_Choose_Game_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
---
+++ QT_Choose_Game/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/CMakeFiles/Choose_Game.dir/Choose_Game_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/Choose_Game.dir/Choose_Game_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
+ C:\Users\User\Documents\QT_Projects\Choose_Game\build\Desktop_Qt_6_7_1_MinGW_64_bit-Debug\Choose_Game_autogen\EWIEGA46WW\qrc_resources.cpp
